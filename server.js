5b8352c2163e00ad63082dd16a5311422cfff74e
Gillian Baker <gillianbaker97@gmail.com>
2021-08-30 21:46:23 -0400

    hosting

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
+//const uri="mongodb+srv://AtlasAdmin:<koalalove97>@cluster0.vjzey.mongodb.net/Exercises?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,6 +29,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Exercises", { u
 db.Exercises.create({name: "Daily Workout"})
   .then(dbExercises => {
     console.log(dbExercises);
+    console.log("your workouts were created!");
   })
   .catch(({message}) => {
     console.log(message);
@@ -31,9 +41,11 @@ app.post("/Exercises", ({body}, res) => {
     .then(({_id}) => db.Exercises.findOneAndUpdate({}, {$push: { workouts: _id}}, {new: true}))
     .then(dbExercises => {
       res.json(dbExercises);
+      console.log("your workout was updated!");
     })
     .catch(err => {
       res.json(err);
+      console.log("your workout failed to update; please try again");
     });
 });
 
@@ -42,6 +54,7 @@ app.get("/Exercises", (req, res) => {
   db.Exercises.find({})
   .then(dbExercises => {
     res.json(dbExercises);
+    console.log("found the workouts!")
   })
   .catch(err => {
     res.json(err);
@@ -53,6 +66,7 @@ app.get("/Exercises", (req, res) => {
   db.Exercises.find({})
   .then( dbExercises => {
     res.json(dbExercises);
+    console.log("exercise retrieved!");
   })
   .catch(err => {
     res.json(err)
@@ -62,3 +76,7 @@ app.get("/Exercises", (req, res) => {
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
+
+
